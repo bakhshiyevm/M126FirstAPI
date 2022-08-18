89d8a6627d08ad2d22eb84846fd1de0ede05270c
BakhshiyevM <mbakhshiyev@pasha-insurance.az>
2022-08-18 19:29:02 +0400

    Add project files.

---
+++ Presentation/Controllers/UserController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Presentation.Model;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Presentation.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        [HttpGet]
+        [Route("getUsers")]
+        public IEnumerable<User> Get() 
+        {
+            List<User> users = new List<User>();
+
+            users.Add(new User {Id=1,Name="Test" });
+            users.Add(new User {Id=2,Name="Test2" });
+
+            return users;
+        }
+
+        [HttpGet]
+        [Route("getUser/{id}")]
+        public User Get([FromRoute] int id)
+        {
+            var user = new User { Id = 1, Name = "Test" };
+
+            return user;
+        }
+
+        [HttpPost]
+        [Route("createUser")]
+        public void Create([FromBody] User user)
+        {
+
+        }
+
+        [HttpPut]
+        [Route("updateUser")]
+        public void Update([FromBody] User user)
+        {
+
+        }
+
+        [HttpDelete]
+        [Route("deleteUser/{id}")]
+        public void Delete([FromRoute] int id)
+        {
+
+        }
+    }
+}
---
+++ Presentation/Model/User.cs
@@ -0,0 +1,8 @@
+﻿namespace Presentation.Model
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
